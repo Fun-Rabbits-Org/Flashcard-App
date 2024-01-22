3ab45a1c133d826d29102ed502062a5db64dda3a
lumengli7 <131196081+lumengli7@users.noreply.github.com>
2024-01-22 16:17:16 -0600

    added login/register routes

---
+++ server/router/deckrouter.js
@@ -0,0 +1,21 @@
+// const express = require('express');
+// const router = express.Router();
+// const deckController = require('../controllers/DeckController');
+
+// router.get('/', deckController.getDecks, (req, res) => {
+//   return res.status(200).json(res.locals.getDecks);
+// });
+
+// router.post('/', deckController.postDecks, (req, res) => {
+//   return res.status(200).json(res.locals.newDeck);
+// });
+
+// // deckRrouter.delete('/deck/:deckId', deckController.deleteDeck, (req, res) => {
+// //   return res.status(200).json(res.locals.message);
+// // });
+
+// // deckRrouter.get('/deck/:deckId', deckController.getDeckById, (req, res) => {
+// //   return res.redirect(200, '/deck/:deckId/card');
+// // });
+
+// module.exports = router;
---
+++ server/router/userRouter.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+const userController = require('../controllers/userController');
+
+router.post('/register', userController.createUser, (req, res) => {
+  return res.status(200).json(res.locals.user);
+});
+
+router.post('/login', userController.verifyUser, (req, res) => {
+  return res.status(200).json(res.locals.existingUser);
+});
+
+module.exports = router;
