314bfe5d81297e866b82396cc593e3a1949c1d11
cyburns <72774499+cyburns@users.noreply.github.com>
2024-01-23 14:31:06 -0500

    cc

--- client/components/Deck/Deck.jsx
+++ client/components/Deck/Deck.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import { getDecks } from '../../utils/requests';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { getDecks } from "../../utils/requests";
 
 const Deck = ({ deck, index }) => {
   const navigate = useNavigate();
@@ -11,23 +11,26 @@ const Deck = ({ deck, index }) => {
   };
 
   const handleDelete = async () => {
-    const response = await fetch(`http://localhost:3000/deck/${deck._id}`, {
-      method: 'DELETE',
-      headers: { 'Content-Type': 'application/json' },
-    });
+    try {
+      const response = await fetch(`http://localhost:3000/deck/${deck._id}`, {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
+      });
 
-    // invoke getDecks
-    if (response.status === 200) getDecks();
+      if (response.status === 200) getDecks();
+    } catch (error) {
+      console.log(error, "error getting decks");
+    }
   };
 
   const colorsArray = [
-    '#00A7ED',
-    '#8361F4',
-    '#E75552',
-    '#EA9823',
-    '#87CE45',
-    '#93D5F3',
-    '#29BDB6',
+    "#00A7ED",
+    "#8361F4",
+    "#E75552",
+    "#EA9823",
+    "#87CE45",
+    "#93D5F3",
+    "#29BDB6",
   ];
 
   const styles = {
@@ -36,9 +39,9 @@ const Deck = ({ deck, index }) => {
   };
 
   return (
-    <div id={`deck${index}`} className='Deck'>
-      <div className='deckColor' onClick={handleDeckClick} style={styles}>
-        <h2>{deck.deckName}</h2>
+    <div id={`deck${index}`} className="Deck">
+      <div className="deckColor" onClick={handleDeckClick} style={styles}>
+        <h2>{deck.deckName} </h2>
       </div>
 
       <button onClick={handleDelete}>Delete</button>
